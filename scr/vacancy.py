7cdd2aba7a8788cefdffa00f2e84290db19399ca
AlexandrBxbrxv <g1026@bk.ru>
2024-08-02 22:03:34 +0500

    + метод превращения объекта в список

--- scr/vacancy.py
+++ scr/vacancy.py
@@ -6,15 +6,16 @@ class Vacancy:
     salary: dict
     alternate_url: str
 
-    "вакансий с указанием названия компании, названия вакансии и зарплаты и ссылки на вакансию."
-
     def __init__(self, employer_id, vacancy_id, name, salary, alternate_url):
         self.employer_id = employer_id['id']
         self.vacancy_id = vacancy_id
         self.name = name
-        self.salary = salary if salary else '-'
-        self.pay_from = self.salary['from'] if self.salary['from'] else self.salary['to']
+        self.salary = salary if salary else {}
+        self.pay = self.salary.get('from') if self.salary.get('from') else self.salary.get('to')
         self.alternate_url = alternate_url if alternate_url else '-'
+
+    def __str__(self):
+        return f'{self.vacancy_id}, {self.name}, {self.pay}, {self.alternate_url}'
 
     @classmethod
     def cast_to_object(cls, employers_vacancies):
@@ -22,20 +23,21 @@ class Vacancy:
         vacancies_list = []
         for employer_vacancies in employers_vacancies:
             for vacancy in employer_vacancies:
-                if vacancy.get('salary') is None:
-                    continue
-                else:
-                    vacancy_object = Vacancy(
-                        vacancy.get('employer'),
-                        vacancy.get('id'),
-                        vacancy.get('name'),
-                        vacancy.get('salary'),
-                        vacancy.get('alternate_url')
-                    )
+                vacancy_object = Vacancy(
+                    vacancy.get('employer'),
+                    vacancy.get('id'),
+                    vacancy.get('name'),
+                    vacancy.get('salary'),
+                    vacancy.get('alternate_url')
+                )
+                if vacancy_object.salary != {}:
                     vacancies_list.append(vacancy_object)
 
         return vacancies_list
 
     @classmethod
     def cast_object_to_list(cls, vacancies_list):
-        pass
+        vacancies_values = []
+        for vacancy in vacancies_list:
+            values = [vacancy.employer_id, vacancy.vacancy_id, vacancy.name, vacancy.pay, vacancy.alternate_url]
+            vacancies_values.append(values)
