91002faa84665ec0f75d20e80dbe2b14d16a28dc
AlexandrBxbrxv <g1026@bk.ru>
2024-08-08 15:10:16 +0500

    + первым значением идёт названия колонок таблицы

--- scr/func_vacancy_converter.py
+++ scr/func_vacancy_converter.py
@@ -1,7 +1,10 @@
 def convert_vacancies_to_lists(employers_vacancies: list) -> list[list]:
     """Собирает основные значения вакансии,
-     выбирает только то вакансии в которых указана минимальная зарплата в рублях"""
+     выбирает только то вакансии в которых указана минимальная зарплата в рублях,
+     первая[0] строчка - название колонок таблицы"""
     vacancies_values_list = []
+    header = ['employer_id', 'vacancy_id', 'name', 'pay', 'alternate_url']
+    vacancies_values_list.append(header)
     for vacancy in employers_vacancies:
         employer = vacancy.get('employer')
         salary = vacancy.get('salary') if vacancy.get('salary') else {'from': 0, 'currency': 'empty_salary'}
