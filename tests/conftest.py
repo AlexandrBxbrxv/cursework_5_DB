7ff21ca2337b4aa0cb96ec090fdd775c21347e43
AlexandrBxbrxv <g1026@bk.ru>
2024-08-09 16:07:17 +0500

    + возвращение данных из конфига для БД

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,6 @@
 import pytest
 import json
+from config import config_db
 
 
 @pytest.fixture()
@@ -16,3 +17,9 @@ def vacancies() -> list[dict]:
     with open(r'C:\Users\Alexsandr\PycharmProjects\new_coursework_5_DB\data\test_vacancies.json', 'r', encoding='UTF-8') as f:
         result = json.loads(f.read())
     return result
+
+
+@pytest.fixture()
+def db_init():
+    """Возвращает данные подключения к postgresql"""
+    return config_db()
