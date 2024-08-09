fa6295cfeed52e6aad5cd3342b241fa25da5995e
AlexandrBxbrxv <g1026@bk.ru>
2024-08-09 16:06:35 +0500

    + функция получающая данные для БД из файла .ini

---
+++ config.py
@@ -0,0 +1,18 @@
+from configparser import ConfigParser
+
+
+def config_db(file_name='database.ini'):
+    """Конфиг специально настроен для работы с классом DBManager,
+    возвращает 4 значения необходимые для инициализации"""
+    parser = ConfigParser()
+    parser.read(file_name)
+    db = {}
+    if parser.has_section('postgresql'):
+        params = parser.items('postgresql')
+        for param in params:
+            db[param[0]] = param[1]
+    else:
+        raise Exception(
+            'Section {0} is not found in {1} file.'.format('postgresql', file_name)
+        )
+    return db['password'], db['user'], db['port'], db['host']
