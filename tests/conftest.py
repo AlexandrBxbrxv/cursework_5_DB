cbeadcdbb313d3dbc159607491e75dd6ebe859ff
AlexandrBxbrxv <g1026@bk.ru>
2024-08-09 19:28:14 +0500

    + фикстура с созданными таблицами, + заполненными таблицами

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,8 @@
 import pytest
 import json
 from config import config_db
+from scr.db_manager import DBManager
+from scr.func_converters import *
 
 
 @pytest.fixture()
@@ -24,3 +26,21 @@ def db_init() -> tuple:
     """Возвращает данные подключения к postgresql,
     перед вызовом ставить *"""
     return config_db()
+
+
+@pytest.fixture()
+def db_created_tables(db_init):
+    """Возвращает объект класса DBManager, с созданными таблицами"""
+    db = DBManager(*db_init)
+    db.create_table_employers()
+    db.create_table_vacancies()
+    return db
+
+
+@pytest.fixture()
+def db_filled_tables(db_created_tables, employers, vacancies):
+    """Возвращает объект класса DBManager, с заполненными таблицами"""
+    db = db_created_tables
+    db.fill_table('employers', convert_employers_to_lists(employers))
+    db.fill_table('vacancies', convert_vacancies_to_lists(vacancies))
+    return db
