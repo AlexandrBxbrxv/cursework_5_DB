c0edb2351183bfc00974120cfdec3f78080bae11
AlexandrBxbrxv <g1026@bk.ru>
2024-08-09 20:45:01 +0500

    замержило всё что было в develop ветке, лишнее для ветки main - удалено

--- tests/test_hh_api_vacancies.py
+++
@@ -1,9 +0,0 @@
-from scr.hh_api_vacancies import HHapiVacancies
-
-
-def test_hh_api_vacancies(employers):
-    """Проверяет типы результата загрузки вакансий"""
-    hh_vac = HHapiVacancies()
-    vacancies = hh_vac.load_vacancies(employers)
-    assert type(vacancies) is list
-    assert type(vacancies[0]) is dict
